0381449d6599f9ac15f01055f1e88023d69a717a
Joakim Grahn <joakim.grahn@qapital.com>
2018-12-05 10:40:51 +0100

    Split day 5 logic

--- 2018/5/part2.rb
+++ 2018/5/part2.rb
@@ -1,19 +1,7 @@
+load 'reaction.rb'
+
 input_data = File.readlines('input.txt')[0]
 alphabet = ('a'..'z').to_a
-
-def react_with_adjacent_units(data)
-    remainder = []
-    while (data.length > 0) do
-        if (data[-2] == nil); break end
-        if (data[-1] != data[-2] && data[-1].casecmp(data[-2]) == 0)
-            data.pop(2)
-            data = data << remainder.pop() unless remainder[-1].nil?
-            next
-        end
-        remainder << data.pop()
-    end
-    return data.length + remainder.length
-end
 
 results = {}
 alphabet.each { |letter|
---
+++ 2018/5/reaction.rb
@@ -0,0 +1,13 @@
+def react_with_adjacent_units(data)
+    remainder = []
+    while (data.length > 0) do
+        if (data[-2] == nil); break end
+        if (data[-1] != data[-2] && data[-1].casecmp(data[-2]) == 0)
+            data.pop(2)
+            data = data << remainder.pop() unless remainder[-1].nil?
+            next
+        end
+        remainder << data.pop()
+    end
+    return data.length + remainder.length
+end
