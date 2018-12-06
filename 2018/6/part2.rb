3e97c5d975227a9f8fc5f674c7e816ecf9785ce9
Joakim Grahn <joakim.grahn@qapital.com>
2018-12-06 22:51:13 +0100

    More mods day 6 pt2

--- 2018/6/part2.rb
+++ 2018/6/part2.rb
@@ -1,9 +1,9 @@
 load 'common.rb'
 
-min_dist = {}
+min_dist = 0
 $x_range.each do |x|
     $y_range.each do |y|
-        min_dist[[x,y]] = $coords.sum { |coord| manhattan_distance(Point.new(x,y), coord) }
+        min_dist += 1 unless $coords.sum { |coord| manhattan_distance(Point.new(x,y), coord) } >= 10000
     end
 end
-puts min_dist.select { |_, dist| dist < 10000}.length
+puts min_dist
