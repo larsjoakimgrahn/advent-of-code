bca0422cf93167ffe805033329dc859f6c38eb17
Joakim Grahn <joakim.grahn@qapital.com>
2018-12-06 22:38:34 +0100

    Minimize day 6 part 2

--- 2018/6/part2.rb
+++ 2018/6/part2.rb
@@ -3,12 +3,7 @@ load 'common.rb'
 min_dist = {}
 $x_range.each do |x|
     $y_range.each do |y|
-        dist = 0
-        location = Point.new(x,y)
-        $coords.each do |coord|
-            dist += manhattan_distance(location, coord)
-        end
-        min_dist[location] = dist
+        min_dist[[x,y]] = $coords.sum { |coord| manhattan_distance(Point.new(x,y), coord) }
     end
 end
 puts min_dist.select { |_, dist| dist < 10000}.length
