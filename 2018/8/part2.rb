4dc9d08497e254f0670f51ae32eea109621b0df8
Joakim Grahn <joakim.grahn@qapital.com>
2018-12-08 22:55:21 +0100

    Clean up day8 part2

--- 2018/8/part2.rb
+++ 2018/8/part2.rb
@@ -1,43 +1,24 @@
 $input_data = File.readlines('input.txt')[0].split().map { |v| v.to_i }
 class TNode
-
+    attr_reader :children
+    attr_writer :metadata
     def initialize
         @children = []
         @metadata = []
     end
 
-    def add_child(child)
-        @children << child
-    end
-
-    def set_metadata(metadata)
-        @metadata = metadata
-    end
-
     def value()
-        if @children.empty?
-            return @metadata.reduce(0, :+)
-        else
-            total_value = 0
-            @metadata.each do |m|
-                if m > @children.size || m == 0
-                    next
-                else
-                    total_value += @children[m-1].value()
-                end
-            end
-            return total_value
-        end
+        @children.empty? ? @metadata.sum : @metadata.map { |m| @children[m-1].value() unless @children[m-1] == nil }.compact.sum
     end
 end
 
 def parse(children, metadata)
     node = TNode.new()
     for _ in 0...(children) do
-        node.add_child(parse($input_data.shift, $input_data.shift))
+        node.children << parse($input_data.shift, $input_data.shift)
     end
 
-    node.set_metadata($input_data.shift(metadata))
+    node.metadata = $input_data.shift(metadata)
     return node
 end
 
