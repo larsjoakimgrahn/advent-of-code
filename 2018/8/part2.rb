296e566ee69a864f525185bfe54c20b0e18b4450
Joakim Grahn <joakim.grahn@qapital.com>
2018-12-08 22:36:02 +0100

    8 part 2

---
+++ 2018/8/part2.rb
@@ -0,0 +1,41 @@
+$input_data = File.readlines('input.txt')[0].split().map { |v| v.to_i }
+class TNode
+
+    def addChild(child)
+        @children ||= []
+        @children << child
+    end
+
+    def set_metadata(metadata)
+        @metadata = metadata
+    end
+
+    def value()
+        if @children == nil
+            return @metadata.reduce(0, :+)
+        else
+            total_value = 0
+            @metadata.each do |m|
+                if m > @children.size || m == 0
+                    next
+                else
+                    total_value += @children[m-1].value()
+                end
+            end
+            return total_value
+        end
+    end
+end
+
+def parse(children, metadata)
+    node = TNode.new()
+    for _ in 0...(children) do
+        node.addChild(parse($input_data.shift, $input_data.shift))
+    end
+
+    node.set_metadata($input_data.shift(metadata))
+    return node
+end
+
+node = parse($input_data.shift, $input_data.shift)
+puts "#{node.value()}"
