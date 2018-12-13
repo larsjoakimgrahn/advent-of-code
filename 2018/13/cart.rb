79d88790c41a600d324be93726d0d7166db267cc
Joakim Grahn <joakim.grahn@qapital.com>
2018-12-13 11:03:42 +0100

    Carts part1

---
+++ 2018/13/cart.rb
@@ -0,0 +1,103 @@
+class Cart
+    attr_reader :x, :y, :direction
+    def initialize(x,y,direction)
+        @x = x
+        @y = y
+        @direction = direction
+        @num_turns = 0
+        @turn_order = ['left', 'straight', 'right']
+    end
+
+    def turn_dir()
+        turn_dir = @turn_order[@num_turns % @turn_order.length]
+        @num_turns += 1
+        return turn_dir
+    end
+
+    def move(grid)
+        case @direction
+        when '^'
+            case grid[[@x,@y-1]]
+            when '\\'
+                @direction = '<'
+            when '/'
+                @direction = '>'
+            when '+'
+                t_dir = turn_dir()
+                case t_dir
+                when 'left'
+                    @direction = '<'
+                when 'right'
+                    @direction = '>'
+                end
+            end
+            @y = @y-1
+            # puts "moving up"
+        when 'v'
+            case grid[[@x,@y+1]]
+            when '\\'
+                @direction = '>'
+            when '/'
+                @direction = '<'
+            when '+'
+                t_dir = turn_dir()
+                case t_dir
+                when 'left'
+                    @direction = '>'
+                when 'right'
+                    @direction = '<'
+                end
+            end
+            @y = @y+1
+            # puts "moving down"
+        when '>'
+            case grid[[@x+1,@y]]
+            when '\\'
+                @direction = 'v'
+            when '/'
+                @direction = '^'
+            when '+'
+                t_dir = turn_dir()
+                case t_dir
+                when 'left'
+                    @direction = '^'
+                when 'right'
+                    @direction = 'v'
+                end
+            end
+            @x = @x+1
+            # puts "moving right"
+        when '<'
+            case grid[[@x-1,@y]]
+            when '\\'
+                @direction = '^'
+            when '/'
+                @direction = 'v'
+            when '+'
+                t_dir = turn_dir()
+                case t_dir
+                when 'left'
+                    @direction = 'v'
+                when 'right'
+                    @direction = '^'
+                end
+            end
+            @x = @x-1
+            # puts "moving left"
+        end
+        return "#{@x},#{@y}"
+    end
+
+    def to_grid()
+        case @direction
+        when '^','v'
+            return '|'
+        when '<','>'
+            return '-'
+        end
+    end
+
+    def eql?(other)
+         @x == other.x && @y == other.y
+    end
+end
---
+++ 2018/13/part1.rb
@@ -0,0 +1,35 @@
+load 'cart.rb'
+data = File.readlines('input.txt')
+
+carts = []
+grid = {}
+data.each_with_index do |line, y|
+    line.split(//).each_with_index do |c, x|
+        case c
+        when '-','|','/','\\','+'
+            grid[[x,y]] = c
+        when '<','>','^','v'
+            cart = Cart.new(x,y,c)
+            carts << cart
+            grid[[x,y]] = cart.to_grid
+        end
+    end
+end
+
+loop do
+    carts.sort! { |a, b| [a.y, a.x] <=> [b.y, b.x] }
+    # puts "#{carts}"
+
+    positions = Hash[carts.map {|c| "#{c.x},#{c.y}" }.collect{|c| [c,true]} ]
+    carts.each do |cart|
+        pos_before = "#{cart.x},#{cart.y}"
+        new_pos = cart.move(grid)
+        if(positions[new_pos] == true)
+            puts "CRASH! = #{new_pos}"
+            exit
+        else
+            positions[pos_before] = false
+            positions[new_pos] = true
+        end
+    end
+end
