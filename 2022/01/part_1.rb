be4a03a610150fad7d512e76e5d44f893019052e
Joakim Grahn <joakim.grahn@qapital.com>
2022-12-01 08:36:03 +0100

    Cleanup

--- 2022/01/part_1.rb
+++ 2022/01/part_1.rb
@@ -1,15 +1,8 @@
 $data = File.readlines('input.txt')
 
-$prevDepth = 100_000_000
-$count = 0
-
-def count(depth)
-  $count += 1 if depth > $prevDepth
-  $prevDepth = depth
-end
-
 $elfs = Hash.new(0)
 $elfcounter = 0
+
 $data.each do |line|
   if line.strip.empty? then 
     $elfcounter += 1
