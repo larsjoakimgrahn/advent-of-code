4e394519fc28f7b87c7663d9fa9e6374804e19c6
Joakim Grahn <joakim.grahn@qapital.com>
2022-12-01 08:30:33 +0100

    2022 day 1

---
+++ 2022/01/part_2.rb
@@ -0,0 +1,21 @@
+$data = File.readlines('input.txt')
+
+$prevDepth = 100_000_000
+$count = 0
+
+def count(depth)
+  $count += 1 if depth > $prevDepth
+  $prevDepth = depth
+end
+
+$elfs = Hash.new(0)
+$elfcounter = 0
+$data.each do |line|
+  if line.strip.empty? then 
+    $elfcounter += 1
+  else
+    $elfs[$elfcounter] += line.to_i
+  end
+end
+
+puts $elfs.values.sort.reverse.first(3).sum
