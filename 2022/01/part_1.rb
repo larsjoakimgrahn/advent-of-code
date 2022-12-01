1356d57e50890926f1b4f9643984e1b53b784fcb
Joakim Grahn <joakim.grahn@qapital.com>
2022-12-01 08:39:00 +0100

    obfuscate code

--- 2022/01/part_1.rb
+++ 2022/01/part_1.rb
@@ -4,11 +4,7 @@ $elfs = Hash.new(0)
 $elfcounter = 0
 
 $data.each do |line|
-  if line.strip.empty? then 
-    $elfcounter += 1
-  else
-    $elfs[$elfcounter] += line.to_i
-  end
+  line.strip.empty? ? $elfcounter += 1 : $elfs[$elfcounter] += line.to_i
 end
 
 puts $elfs.values.max
